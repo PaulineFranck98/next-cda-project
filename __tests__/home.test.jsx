f11455544dabff424660e203a5f1305773731f17
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-06-04 15:44:25 +0200

    feat(CI): ajout des fichier jest et dossier .circleci

---
+++ __tests__/home.test.jsx
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Home from "../app/page";
+
+
+
+  describe("Home Page", () => {
+  it("check for relevant text", () => {
+    const { getByText } = render(<Home />);
+
+    expect(
+      getByText((content) => content.includes("Get started by editing"))
+    ).toBeInTheDocument();
+    
+    expect(
+      getByText((content) => content.includes("Save and see your changes instantly."))
+    ).toBeInTheDocument();
+  });
+});
+
