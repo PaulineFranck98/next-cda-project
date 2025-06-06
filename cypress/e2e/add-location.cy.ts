c03d8740a0ad8a5bac34c1ce634d34d8012ed850
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-06-06 16:42:02 +0200

    feat(ci) : ajout des fichiers de test  (jest, cypress) et modification du config.yml pour intégrer Cypress

---
+++ cypress/e2e/add-location.cy.ts
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Ajout de lieu', () => {
+  it('remplit le formulaire et envoie en bdd un nouveau lieu', () => {
+    
+    cy.visit('/dashboard/location/new-location');
+
+    // remplit les champs
+    cy.get('input[name="locationName"]').type('Mon super lieu');
+    cy.get('input[name="address"]').type('123 rue de test');
+    cy.get('input[name="zipcode"]').type('68000');
+    cy.get('input[name="city"]').type('Colmar');
+    cy.get('input[name="phoneNumber"]').type('0123456789');
+    cy.get('input[name="website"]').type('www.siteTest.com');
+    cy.get('textarea[name="description"]').type('Un lieu super sympa à à Colmar');
+    cy.get('input[name="latitude"]').type('48.8566');
+    cy.get('input[name="longitude"]').type('2.3522');
+
+    // intercepte la requête POST
+    cy.intercept('POST', '/api/location').as('postLocation');
+
+    // submit le formulaire
+    cy.get('form[data-testid="location-form"]').submit();
+
+    // attend la requête API et vérifie qu'elle répond bien 201
+    cy.wait('@postLocation').its('response.statusCode').should('eq', 201);
+
+    // vérifie qu'on est redirigé vers la bonne page
+    // [a-f0-9]{24}  24 caractères héxadécimaux (car c'est ce que MongoDB génère : évite une erreur d'ID ).
+    cy.url().should('match', /\/dashboard\/location\/[a-f0-9]{24}$/); 
+
+    // vérifie que le toast de succès s'affiche
+    cy.contains('Ajouté avec succès');
+  });
+});
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,25 @@
+// ***********************************************************
+// This example support/e2e.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+Cypress.on('uncaught:exception', (err) => {
+  if (err.message.includes("Failed to execute 'removeChild' on 'Node'")) {
+    return false; // Ignore l'exception "removeChild" : pour le toast par exemple
+  }
+
+  return true; // Laisse planter pour les autres erreurs
+});
