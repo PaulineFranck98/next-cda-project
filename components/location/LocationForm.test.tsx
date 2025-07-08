e812d25dbd587f75ed306d63ebf03cd675a932bd
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-07-08 11:54:11 +0200

    (fix) : tets cypress - import jest

--- components/location/LocationForm.test.tsx
+++ components/location/LocationForm.test.tsx
@@ -1,5 +1,5 @@
 
-import "@testing-library/jest-dom/extend-expect";
+// import "@testing-library/jest-dom/extend-expect";
 import { expect } from '@jest/globals';
 
 //  render : pour afficher un composant de un environnement de test
