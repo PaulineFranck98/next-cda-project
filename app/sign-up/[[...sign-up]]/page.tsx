00286d6bfe5f6c9481d3a2bedf0cdc0fbe3495b5
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-07-11 09:09:25 +0200

    (fix) redirection : test avec 'redirectUrl'

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,17 +5,28 @@ import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function SignUpPage() {
-  const { isSignedIn } = useUser();
-  const router = useRouter();
+// export default function SignUpPage() {
+//   const { isSignedIn } = useUser();
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (isSignedIn) {
-      console.log("User is signed in");
-      router.push("/dashboard");
-    } else {
-      console.log("User is not signed in");}
-  }, [isSignedIn, router]);
+//   useEffect(() => {
+//     if (isSignedIn) {
+//       console.log("User is signed in");
+//       router.push("/dashboard");
+//     } else {
+//       console.log("User is not signed in");}
+//   }, [isSignedIn, router]);
 
-  return <SignUp />;
-}
+//   return <SignUp />;
+// }
+
+const SignUpPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1>Sign Up</h1>
+      <SignUp forceRedirectUrl={"/dashboard"}/>
+    </div>
+  );
+};
+
+export default SignUpPage;
