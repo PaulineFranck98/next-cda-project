860c6d6d0ae9191d83a39d9707a4ab8d206a9313
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-09-05 14:38:44 +0200

    (fix)index 2dsphere : correciton du fichier de config pour permettre la connexion à la bdd

--- scripts/create-2dsphere-index.js
+++ scripts/create-2dsphere-index.js
@@ -1,17 +1,16 @@
 const { MongoClient } = require('mongodb');
 
-async function main() {
-  const uri = process.env.DATABASE_URL;
-  const client = new MongoClient(uri);
+async function createIndex() {
+  const client = new MongoClient(process.env.DATABASE_URL);
   try {
     await client.connect();
     const db = client.db(); 
     const collection = db.collection('locations');
     await collection.createIndex({ geo: "2dsphere" });
-    console.log('2dsphere index created on geo');
+
   } finally {
     await client.close();
   }
 }
 
-main().catch(console.error);
+createIndex().catch(console.error);
