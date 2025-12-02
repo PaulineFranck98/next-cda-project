1b2a569c94677d9571166b5a018c5e2477db5e5b
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-12-02 11:32:44 +0100

    fix(test error) : fix du test de la page d'accueil

--- __tests__/home.test.jsx
+++ __tests__/home.test.jsx
@@ -3,17 +3,14 @@ import { render } from "@testing-library/react";
 import Home from "../app/page";
 
 
-  describe("Home Page", () => {
-  it("check for relevant text", () => {
-    const { getByText } = render(<Home />);
+describe("Home Page", () => {
+	it("check for relevant text", () => {
+		const { getByText } = render(<Home />);
 
-    expect(
-      getByText((content) => content.includes("Get started by editing"))
-    ).toBeInTheDocument();
-    
-    expect(
-      getByText((content) => content.includes("Save and see your changes instantly."))
-    ).toBeInTheDocument();
-  });
+		expect(
+			getByText((content) => content.includes("Hello"))
+		).toBeInTheDocument();
+
+	});
 });
 
